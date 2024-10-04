7517419cb88236c07a864336e23a02f5ce50fa93
Mukhlis Abdillah <141989153+mukhlisabdll@users.noreply.github.com>
2024-10-04 23:46:56 +0700

    Delete data-science-practice directory

--- data-science-practice/statistics/latihan2_persentase.py
+++
@@ -1,7 +0,0 @@
-# Latihan 9: Menghitung Persentase Kenaikan dalam Data
-sales = [1000, 1200, 1500, 1800, 2000] # Data penjualan untuk setiap tahun
-
-# Menghitung persentase kenailkan setiap tahun
-for i in range(1, len(sales)): # Looping mulai dari elemen kedua hingga akhir
-    growth = ((sales[i] - sales[i-1]) / (sales[i-1])) * 100 # Menghitung persentase kenaikan
-    print(f"Kenaikan persentase dari tahun {i} ke {i+1}: {growth:.2f}%") # Menampilkan hasil kenaikan dalam persen
